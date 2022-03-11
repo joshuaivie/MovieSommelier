854e47aac61f70887e074404fa40d12906fa5b45
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-03-11 23:08:04 +0100

    Restructure API Folders

--- src/api/Involvement-api.js
+++ src/api/Involvement-api.js
@@ -1,23 +1,30 @@
 const BASE_URL = ' https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
 const APP_ID = 'OAW88uh8tayXubZCqUNx';
 
-export async function like(item_id){
+export async function like(item_id) {
     return await fetch(`${BASE_URL}/apps/${APP_ID}/likes`,
-        { method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({item_id}) }
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ item_id })
+        }
     ).then(response => response.json());
 }
-export async function getLikesList(){
+
+export async function getLikesList() {
     return await fetch(`${BASE_URL}/apps/${APP_ID}/likes`).then(response => response.json());
 }
-export async function comment(item_id, username, comment){
+
+export async function comment(item_id, username, comment) {
     return await fetch(`${BASE_URL}/apps/${APP_ID}/comments`,
-        { method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({item_id, username, comment}) }
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ item_id, username, comment })
+        }
     ).then(response => response.json());
 }
-export async function getCommentList(item_id){
+
+export async function getCommentList(item_id) {
     return await fetch(`${BASE_URL}/apps/${APP_ID}/comments?item_id=${item_id}`).then(response => response.json());
 }
