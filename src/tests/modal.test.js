a8f7573c5f9923d670e857c3af6a7869c7c61bea
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-03-12 21:54:28 +0100

    Fix Linter Errors

--- src/tests/modal.test.js
+++ src/tests/modal.test.js
@@ -1,36 +1,34 @@
 import DetailsModal from '../components/modal';
 
-
-describe("Details Modal", () => {
+describe('Details Modal', () => {
   const modal = new DetailsModal();
-  const countCommentsSpy = jest.spyOn(modal, "countComments");
-
+  const countCommentsSpy = jest.spyOn(modal, 'countComments');
 
   const getComments = jest.fn(() => [
     {
-      "username": "Joshua Ivie",
-      "comment": "This is a test comment",
-      "creation_date": "2022-03-12",
+      username: 'Joshua Ivie',
+      comment: 'This is a test comment',
+      creation_date: '2022-03-12',
     },
     {
-      "username": "Joshua Ivie",
-      "comment": "This is another test comment",
-      "creation_date": "2022-03-12",
-    }
+      username: 'Joshua Ivie',
+      comment: 'This is another test comment',
+      creation_date: '2022-03-12',
+    },
   ]);
 
-  describe(".countComments", () => {
-    test("must be a function", () => {
-      expect(typeof modal.countComments).toBe("function");
+  describe('.countComments', () => {
+    test('must be a function', () => {
+      expect(typeof modal.countComments).toBe('function');
     });
 
-    test("expects comments list to be an array", () => {
-      expect(modal.countComments).toThrow(Error)
+    test('expects comments list to be an array', () => {
+      expect(modal.countComments).toThrow(Error);
       expect(modal.countComments).toThrow('Expected comment list to be an array');
     });
 
-    test("counts the number of comments", () => {
-      const comments = getComments()
+    test('counts the number of comments', () => {
+      const comments = getComments();
       const result = modal.countComments(comments);
 
       expect(countCommentsSpy).toHaveBeenCalledWith(comments);
